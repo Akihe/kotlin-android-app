a99f83fad5504f3a62d3398e89d6a2e9a00b31fe
Akihe <aki.helin@tuni.fi>
2022-05-21 19:27:29 +0300

    comment away all ui related code which was moved to homefragment.kt

--- FoodRecipes/app/src/main/java/fi/tuni/foodrecipes/MainActivity.kt
+++ FoodRecipes/app/src/main/java/fi/tuni/foodrecipes/MainActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import fi.tuni.foodrecipes.favourites.FavouritesFragment
+import fi.tuni.foodrecipes.home.HomeFragment
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -16,6 +19,7 @@ import java.net.URL
 import kotlin.concurrent.thread
 
 
+/*
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Recipe(var id : Int, var title: String? = null, var image: String) {
 
@@ -29,22 +33,27 @@ data class Recipe(var id : Int, var title: String? = null, var image: String) {
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class RecipeJsonObject(var results: MutableList<Recipe>? = null) {
 }
+*/
 
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var fetchButton : Button
     lateinit var bottomNav : BottomNavigationView
+
+/*
+    lateinit var fetchButton : Button
     var myAdapter = CustomAdapter()
     lateinit var recyclerView : RecyclerView
+*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fetchButton = findViewById(R.id.fetchbutton)
         bottomNav = findViewById(R.id.bottom_navigation)
 
+/*
+        fetchButton = findViewById(R.id.fetchbutton)
         recyclerView = findViewById(R.id.recipeList)
 
         recyclerView
@@ -59,27 +68,42 @@ class MainActivity : AppCompatActivity() {
                 recyclerView.adapter = myAdapter
             }
         }
+*/
 
+        val homeFragment = HomeFragment()
+        val favouritesFragment = FavouritesFragment()
 
+        setCurrentFragment(homeFragment)
         bottomNav.setOnItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.page_1 -> {
+                    setCurrentFragment(homeFragment)
                 }
                 R.id.page_2 -> {
+                    setCurrentFragment(favouritesFragment)
+
                 }
             }
             return@setOnItemSelectedListener true
         }
-        fetchButton.setOnClickListener {
+/*        fetchButton.setOnClickListener {
             thread {
                 createObjects()
             }
-        }
+        }*/
     }
     // "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=a84165b11bbe41f0ae6ff525b82eed8e"
     // "https://api.spoonacular.com/recipes/1234/information/?apiKey=a84165b11bbe41f0ae6ff525b82eed8e"
     // "https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2&apiKey=a84165b11bbe41f0ae6ff525b82eed8e"
 
+
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
+/*
     fun fetch (url : String) : String? {
             var result: String? = null
             val url = URL(url)
@@ -107,4 +131,23 @@ class MainActivity : AppCompatActivity() {
         val recipes: MutableList<Recipe>? = myObject.results
         return recipes as MutableList<Recipe>
     }
+*/
 }
+/*
+var apikey = "a84165b11bbe41f0ae6ff525b82eed8e"
+var baseurl = "https://api.spoonacular.com/"
+retrofit = Retrofit.Builder().baseUrl("https://api.spoonacular.com/").build()
+
+var intent = Intent(this, FavouritesActivity::class.java)
+startActivity(intent)
+
+//fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=a84165b11bbe41f0ae6ff525b82eed8e")
+
+fun fetcher(url:String) {
+    //var data = @GET("/food/ingredients/search?query=banana&number=2&sort=calories&sortDirection=desc&apiKey=a84165b11bbe41f0ae6ff525b82eed8e")
+}
+
+viewHolder.itemView.setOnClickListener {Log.d("view", adapterData[position].id.toString())}
+
+
+ */
