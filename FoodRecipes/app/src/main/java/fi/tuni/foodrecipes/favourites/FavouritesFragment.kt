144fc06a28781a57167dc362f457d8d266f88a7a
Akihe <aki.helin@tuni.fi>
2022-05-21 22:09:27 +0300

    read arguments for testing purposes

--- FoodRecipes/app/src/main/java/fi/tuni/foodrecipes/favourites/FavouritesFragment.kt
+++ FoodRecipes/app/src/main/java/fi/tuni/foodrecipes/favourites/FavouritesFragment.kt
@@ -1,7 +1,14 @@
 package fi.tuni.foodrecipes.favourites
 
+import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import fi.tuni.foodrecipes.R
 
 class FavouritesFragment:Fragment(R.layout.fragment_favourites) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println(arguments?.getIntArray("favourites"))
+    }
 }
