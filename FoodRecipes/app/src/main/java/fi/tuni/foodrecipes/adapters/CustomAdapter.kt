1394e7ab7ecff5e66c8de964d534ff50d39327fe
Akihe <aki.helin@tuni.fi>
2022-05-23 18:06:54 +0300

    create new adapter to handle favourite recyclerviews data

---
+++ FoodRecipes/app/src/main/java/fi/tuni/foodrecipes/adapters/CustomAdapter.kt
@@ -0,0 +1,62 @@
+package fi.tuni.foodrecipes
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import fi.tuni.foodrecipes.home.Recipe
+import fi.tuni.foodrecipes.listeners.OnRecipeClickListener
+
+
+class CustomAdapter(private val listener: OnRecipeClickListener) :
+    RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+    private val adapterData = mutableListOf<Recipe>()
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.myTextView)
+        val imageView : ImageView = view.findViewById(R.id.imageView)
+        val button : ImageButton = view.findViewById(R.id.favButton)
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.recipe, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.itemView.setOnClickListener {
+            listener.onRecipeClick(adapterData[position])
+        }
+
+        viewHolder.button.setOnClickListener {
+            listener.onRecipeButtonClick(adapterData[position])
+        }
+        viewHolder.textView.text = adapterData[position].title
+        Picasso.get().load(adapterData[position].image).into(viewHolder.imageView)
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = adapterData.size
+
+    fun setData(data: List<Recipe>) {
+        adapterData.apply {
+            clear()
+            addAll(data)
+        }
+    }
+}
--- FoodRecipes/app/src/main/java/fi/tuni/foodrecipes/adapters/FavouritesAdapter.kt
+++ FoodRecipes/app/src/main/java/fi/tuni/foodrecipes/adapters/FavouritesAdapter.kt
@@ -1,53 +1,34 @@
-package fi.tuni.foodrecipes
+package fi.tuni.foodrecipes.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
-import fi.tuni.foodrecipes.home.OnRecipeClickListener
+import fi.tuni.foodrecipes.R
 import fi.tuni.foodrecipes.home.Recipe
+import fi.tuni.foodrecipes.listeners.FavouriteListListener
 
-
-class CustomAdapter(val listener: OnRecipeClickListener) :
+class FavouritesAdapter(private val listener: FavouriteListListener) :
     RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
     private val adapterData = mutableListOf<Recipe>()
 
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder).
-     */
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.myTextView)
-        val imageView : ImageView = view.findViewById(R.id.imageView)
-        val button : ImageButton = view.findViewById(R.id.favButton)
-
-    }
-
     // Create new views (invoked by the layout manager)
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): CustomAdapter.ViewHolder {
         // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.recipe, viewGroup, false)
 
-        return ViewHolder(view)
+        return CustomAdapter.ViewHolder(view)
     }
 
     // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(viewHolder: CustomAdapter.ViewHolder, position: Int) {
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        viewHolder.itemView.setOnClickListener {
-            listener.onRecipeClick(adapterData[position])
+        viewHolder.button.setOnClickListener {
+            listener.deleteFavourite(adapterData[position])
         }
 
-        viewHolder.button.setOnClickListener {
-            listener.onRecipeButtonClick(adapterData[position].id)
-        }
         viewHolder.textView.text = adapterData[position].title
         Picasso.get().load(adapterData[position].image).into(viewHolder.imageView)
     }
