bb77a07b3aa0b2aef36012143003bb836bcace91
Akihe <aki.helin@tuni.fi>
2022-05-23 18:03:10 +0300

    move onRecipeClickListener to a new file, modify implemented functions. Use created viewModel to transfer data between fragments

--- FoodRecipes/app/src/main/java/fi/tuni/foodrecipes/home/HomeFragment.kt
+++ FoodRecipes/app/src/main/java/fi/tuni/foodrecipes/home/HomeFragment.kt
@@ -1,20 +1,25 @@
 package fi.tuni.foodrecipes.home
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Parcelable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.databind.ObjectMapper
 import fi.tuni.foodrecipes.CustomAdapter
 import fi.tuni.foodrecipes.R
+import fi.tuni.foodrecipes.SharedViewModel
+import fi.tuni.foodrecipes.listeners.OnRecipeClickListener
 import fi.tuni.foodrecipes.recipe.RecipeDetailsFragment
 import org.json.JSONObject
 import java.io.BufferedReader
@@ -39,18 +44,23 @@ data class RecipeJsonObject(var results: MutableList<Recipe>? = null) {
 
 class HomeFragment : Fragment(R.layout.fragment_home), OnRecipeClickListener {
 
-    lateinit var fetchButton : Button
+    private lateinit var editText: EditText
+    private lateinit var fetchButton : Button
     private var myAdapter = CustomAdapter(this)
     lateinit var recyclerView : RecyclerView
+    private var favourites = arrayListOf<Recipe>()
+    private val model: SharedViewModel by activityViewModels()
 
+
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        super.onCreate(savedInstanceState)
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        editText = view.findViewById(R.id.inputField)
         fetchButton = view.findViewById(R.id.fetchbutton)
         recyclerView = view.findViewById(R.id.recipeList)
 
@@ -60,8 +70,6 @@ class HomeFragment : Fragment(R.layout.fragment_home), OnRecipeClickListener {
             }
 
             thread {
-                var data = createObjects()
-                myAdapter.setData(data)
                 activity?.runOnUiThread {
                     recyclerView.adapter = myAdapter
                 }
@@ -69,17 +77,18 @@ class HomeFragment : Fragment(R.layout.fragment_home), OnRecipeClickListener {
 
         fetchButton.setOnClickListener {
             thread {
-                createObjects()
+                val input = editText.text.toString()
+                val data = createObjects("https://api.spoonacular.com/recipes/complexSearch?query=${input}&number=1&apiKey=a84165b11bbe41f0ae6ff525b82eed8e")
+                Log.d("data", data.toString())
+                activity?.runOnUiThread {
+                    myAdapter.setData(data)
+                    myAdapter.notifyDataSetChanged()
+                }
             }
         }
-
         Log.d("asd", arguments?.getString("message").toString())
-        arguments?.putString("message", "viesti vaihdettu")
-
         return view
     }
-
-
 
     fun fetch (url : String) : String? {
         var result: String? = null
@@ -98,82 +107,36 @@ class HomeFragment : Fragment(R.layout.fragment_home), OnRecipeClickListener {
         }
         return result
     }
+//"https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&apiKey=a84165b11bbe41f0ae6ff525b82eed8e"
+    fun createObjects (url : String) : MutableList<Recipe> {
 
-    fun createObjects () : MutableList<Recipe> {
+        //val data = fetch(url)
+        val data = myData.toString()
 
-        //var data = fetch("https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&apiKey=a84165b11bbe41f0ae6ff525b82eed8e")
-
-        var data = data.toString()
         val mp = ObjectMapper()
         val myObject: RecipeJsonObject = mp.readValue(data, RecipeJsonObject::class.java)
         val recipes: MutableList<Recipe>? = myObject.results
         return recipes as MutableList<Recipe>
     }
 
-    override fun onRecipeButtonClick(id: Int) {
-        Log.d("id", id.toString())
+    override fun onRecipeButtonClick(recipe: Recipe) {
+        favourites.add(recipe)
+        model.addFavouriteRecipe(recipe)
     }
 
     override fun onRecipeClick(recipe: Recipe) {
-        arguments?.putInt("id", recipe.id)
         activity?.supportFragmentManager?.beginTransaction()?.apply {
             addToBackStack(HomeFragment().javaClass.canonicalName)//optional
-            replace(R.id.flFragment, RecipeDetailsFragment())
+            replace(R.id.flFragment, RecipeDetailsFragment(recipe.id))
             commit()
         }
     }
 }
 
-interface OnRecipeClickListener {
-    fun onRecipeButtonClick(id : Int)
-    fun onRecipeClick(recipe : Recipe)
-}
-
-
-var data = JSONObject("""{
+var myData = JSONObject("""{
     "offset": 0,
     "number": 2,
     "results": [
-    {
-        "id": 716429,
-        "title": "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
-        "calories": 584,
-        "carbs": "84g",
-        "fat": "20g",
-        "image": "https://spoonacular.com/recipeImages/716429-312x231.jpg",
-        "imageType": "jpg",
-        "protein": "19g"
-    },
-    {
-        "id": 715538,
-        "title": "What to make for dinner tonight?? Bruschetta Style Pork & Pasta",
-        "calories": 521,
-        "carbs": "69g",
-        "fat": "10g",
-        "image": "https://spoonacular.com/recipeImages/715538-312x231.jpg",
-        "imageType": "jpg",
-        "protein": "35g"
-    }, 
-    {
-        "id": 716429,
-        "title": "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
-        "calories": 584,
-        "carbs": "84g",
-        "fat": "20g",
-        "image": "https://spoonacular.com/recipeImages/716429-312x231.jpg",
-        "imageType": "jpg",
-        "protein": "19g"
-    },
-    {
-        "id": 715538,
-        "title": "What to make for dinner tonight?? Bruschetta Style Pork & Pasta",
-        "calories": 521,
-        "carbs": "69g",
-        "fat": "10g",
-        "image": "https://spoonacular.com/recipeImages/715538-312x231.jpg",
-        "imageType": "jpg",
-        "protein": "35g"
-    },
     {
         "id": 716429,
         "title": "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
